dbfc3f34572b53da24fa7dc914288e5baf05ff6a
유병우 <conner903@naver.com>
2022-06-06 16:47:45 +0900

    feat: save post to posts in store

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="Post-Card">
-    <div>{title}</div>
+    <h4>{title}</h4>
     <div>{content}</div>
 </div>
 
@@ -13,5 +13,16 @@
         width: 20rem;
         border-radius: 4px;
         box-shadow: rgb(0 0 0 / 4%) 0px 4px 16px 0px;
+        padding: 1em;
+
+        >h4 {
+            font-size: 1rem;
+            margin: 0px 0px 0.25rem;
+            line-height: 1.5;
+            word-break: break-word;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            overflow: hidden;
+        }
     }
 </style>

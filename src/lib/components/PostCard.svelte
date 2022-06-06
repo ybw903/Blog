f431bfc1ae84973a4ec05971ab2eeb0b861c6d39
유병우 <conner903@naver.com>
2022-06-06 14:09:41 +0900

    feat: extract post card component

---
+++ src/lib/components/PostCard.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let title;
+    export let content;
+</script>
+
+<div class="Post-Card">
+    <div>{title}</div>
+    <div>{content}</div>
+</div>
+
+<style lang="scss">
+    .Post-Card {
+        width: 20rem;
+        border-radius: 4px;
+        box-shadow: rgb(0 0 0 / 4%) 0px 4px 16px 0px;
+    }
+</style>

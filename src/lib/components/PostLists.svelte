81c87e1c6cf9416d21fcba952f3cae4742d70ab5
유병우 <conner903@naver.com>
2022-06-06 14:09:57 +0900

    style: post container

--- src/lib/components/PostLists.svelte
+++ src/lib/components/PostLists.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
-    let dummyData = [{title: 'test1', description: 'test1'},{title: 'test2', description: 'test2'}]
+import PostCard from "./PostCard.svelte";
+
+    let dummyData = [{title: 'test1', content: 'test1'},{title: 'test2', content: 'test2'}]
 </script>
 <section>
-    <div>
+    <div class="Posts-Container">
         {#each dummyData as data, index}
-            <div>{data.title}</div>
-            <div>{data.description}</div>
+            <PostCard title={data.title} content={data.content}/>
         {/each}
     </div>
 </section>
 
 <style lang="scss">
-    
+    .Posts-Container {
+        display: flex;
+        flex-wrap: wrap;
+    }
 </style>

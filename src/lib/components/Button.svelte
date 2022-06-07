d8f55c98329442e5a7dbd2a63ea11dc54edc1d81
유병우 <conner903@naver.com>
2022-06-07 20:49:12 +0900

    feat: button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let content;
+</script>
+
+<button class="Btn" >{content}</button>
+
+<style lang="scss">
+    .Btn {
+      text-decoration: none;
+      font-size:1rem;
+      color:white;
+      padding:10px 20px 10px 20px;
+      border-radius: 10px;
+      border: none;
+      background: blue;
+      cursor: pointer;
+
+    }
+</style>

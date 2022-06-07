fcbd0acbef09d24cee9c0cb16412a9af23b39b22
유병우 <conner903@naver.com>
2022-06-07 20:50:42 +0900

    refactor: link with Button component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,13 @@
+<script>
+    import Button from './Button.svelte'
+</script>
 
 <header class="Header">
     <div class="Header-Box">
         <h2>Daylog</h2>
-        <a href="#/write">새 글 작성</a>   
+        <a href="#/write">
+            <Button content={"새 글 작성" }/>
+        </a>   
     </div>
 </header>
 
